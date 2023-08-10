256371e9ab254d5a0104a8f5c97a7d128f9d26cc
AbenezerSeyoum <abenezer.sym@gmail.com>
2023-08-10 10:28:45 -0700

    My first C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that pritns a line with put function
  * Retur:0 (Success)
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
