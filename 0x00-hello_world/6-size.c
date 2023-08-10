d4f4c3f126b40aa2a9f0663664170cc1e2f6dc8e
AbenezerSeyoum <abenezer.sym@gmail.com>
2023-08-10 12:26:37 -0700

    My first C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,6 @@
  * main - A program that prints sizes
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 
@@ -13,11 +12,11 @@ int main(void)
 	long int l;
 	long long int z;
 
-	printf("The size of int is: %lu bytes(s)\n" , (unsigned long)sizeof(i);
-	printf("The size of float is: %lu bytes(s)\n" , (unsigned long)sizeof(f);
-	printf("The size of char is: %lu bytes(s)\n" , (unsigned long)sizeof(c);
-	printf("The size of long int is: %lu bytes(s)\n" , (unsigned long)sizeof(l);
-	printf("The size of long long int is: %lu bytes(s)\n" , (unsigned long)sizeof(z);
+	printf("The size of int is: %lu bytes(s)\n" , (unsigned long)sizeof(i));
+	printf("The size of float is: %lu bytes(s)\n" , (unsigned long)sizeof(f));
+	printf("The size of char is: %lu bytes(s)\n" , (unsigned long)sizeof(c));
+	printf("The size of long int is: %lu bytes(s)\n" , (unsigned long)sizeof(l));
+	printf("The size of long long int is: %lu bytes(s)\n" , (unsigned long)sizeof(z));
 	
 	return (0);
 }
