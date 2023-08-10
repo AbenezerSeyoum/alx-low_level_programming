72cfa06911275f4991ee95c3f3f1d8f8bfe0879c
AbenezerSeyoum <abenezer.sym@gmail.com>
2023-08-10 11:02:25 -0700

    My first C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 {
 
 	int i;
-	double d;
+	float f;
 	char c;
 
 	printf("The size of int is: %lu.\n" , (unsigned long)sizeof(i);
-	printf("The size of double is: %lu.\n" , (unsigned long)sizeof(d);
+	printf("The size of float is: %lu.\n" , (unsigned long)sizeof(f);
 	printf("The size of char is: %lu.\n" , (unsigned long)sizeof(c);
 	
 	return (0);
