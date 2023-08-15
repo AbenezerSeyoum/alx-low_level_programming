4aabda002d56e033bf7a6bebbd736b0ba50bae51
AbenezerSeyoum <abenezer.sym@gmail.com>
2023-08-15 13:38:52 -0700

    updatedC2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+_puthcar('_');
+_putchar('p');
+_putchar('u');
+_puthcar('t');
+_puthcar('c');
+_puthcar('h');
+_puthcar('a');
+_puthcar('r');
+_puthcar('\n');
+return (0);
+}
